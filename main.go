2c937dc20907d5ecd6fb571a20cdb4f61f65fc11
George I. Tsopouridis <gtsopour@gmail.com>
2020-02-21 18:03:50 +0100

    Update main.go

--- main.go
+++ main.go
@@ -27,6 +27,15 @@ func fibonacci() func() int {
 		return number
 	}}
 
+func _fibonacci() func() int {
+	first, second := 0, 1
+		return func() int {
+			ret := first
+			first, second = second, first+second
+			return ret
+	}
+}
+
 func main() {
 	f := fibonacci()
 	for i := 0; i < 30; i++ {
